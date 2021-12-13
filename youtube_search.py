9f57cccaa786eb1eb5a78a9b8b66ef46d4ac02bb
myNameArnav <arnjain3@gmail.com>
2021-12-13 18:29:50 +0530

    hid the api key

--- youtube_search.py
+++ youtube_search.py
@@ -1,8 +1,9 @@
-
+from key import api_key
 from apiclient.discovery import build
-api_key  = "AIzaSyBnHr12yY-ZIYLdwdjm1XVkGRMP9b1ih0w"
-youtube  = build('youtube','v3',developerKey=api_key)
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
 def search_vid(query):
-    req = youtube.search().list(q=query, part = 'snippet', type = 'video')
-    res  =req.execute()
-    return res['items'][0]['id']['videoId'],res['items'][0]['snippet']['title']
+    req = youtube.search().list(q=query, part='snippet', type='video')
+    res = req.execute()
+    return res['items'][0]['id']['videoId'], res['items'][0]['snippet']['title']
